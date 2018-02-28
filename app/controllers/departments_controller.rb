b4c89ea1e3a3911000cddffaa5138d4222305be5
Andrew Street <andrew_street@live.com>
2018-02-28 16:48:57 -0700

    departments and items

---
+++ app/controllers/departments_controller.rb
@@ -0,0 +1,49 @@
+class DepartmentsController < ApplicationController
+  before_action :set_department, only: [:show, :edit, :update]
+  def index
+    @departments = Department.all
+  end
+
+  def show
+  end
+
+  def new
+    @department = Department.new 
+  end
+
+  def edit
+  end
+
+  def create
+    @department = Department.new(department_params)
+    if @department.save
+      redirect_to department_path(@department)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @department.update(department_params)
+      redirect_to departments_path(@department)
+    else
+      render :edit
+    end
+  end
+    
+  def destroy
+    @department.destroy
+    redirect_to department_path(@department)
+  end
+
+  private
+
+  def department_params
+    params.require(:department).permit(:name)
+  end
+
+  def set_department
+    @department = Department.find(params[:id])
+  end
+
+end
